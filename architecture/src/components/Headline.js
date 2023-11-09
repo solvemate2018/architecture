5d70fbc6be6164dfe8441ad0c196d17b1aa6e174
solvemate2018 <35694266+solvemate2018@users.noreply.github.com>
2023-11-09 15:19:21 +0100

    Initial commit

---
+++ architecture/src/components/Headline.js
@@ -0,0 +1,12 @@
+export function Headline(props) {
+    return (
+            <div className="flex justify-center items-center flex-col h-16 mb-8 text-secondary">
+                <div className="lg:w-1/2 md:w-5/6 w-full text-center">
+                <h1 className="font-sagoe text-2xl">{props.title}</h1>
+                <h3 className="font-sagoe text-l">
+                    {props.subtitle}
+                </h3>
+                </div>
+            </div>
+    )
+}
---
+++ architecture/src/pages/index.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { Home } from '../sections/Home'
+import { About } from '../sections/About'
+import { Team } from '../sections/Team'
+import { Projects } from '../sections/Projects'
+import { Services } from '../sections/Services'
+import { Contact } from '../sections/Contact'
+import { Footer } from '../sections/Footer'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function HomePage() {
+  return (
+    <main>
+      <Home></Home>
+      <About></About>
+      <Team></Team>
+      <Projects></Projects>
+      <Services></Services>
+      <Contact></Contact>
+      <Footer></Footer>
+    </main>
+  )
+}
