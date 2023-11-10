8bc787fca523368682f290fc0ec4620d44f7cfdd
solvemate2018 <35694266+solvemate2018@users.noreply.github.com>
2023-11-10 15:12:04 +0100

    Added faavicons

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,13 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function HomePage() {
   return (
+    <>
+    <head>
+      <link rel="apple-touch-icon" sizes="180x180" href="./favicons/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="./favicons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="./favicons/favicon-16x16.png" />
+      <link rel="manifest" href="./favicons/site.webmanifest" />
+    </head>
     <main>
       <Home></Home>
       <About></About>
@@ -21,5 +28,6 @@ export default function HomePage() {
       <Contact></Contact>
       <Footer></Footer>
     </main>
+    </>
   )
 }
